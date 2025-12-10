e99d86eb0bc93d0f5577fbc0ce30d7fee444b5f2
Diego Saravia <127380387+Soyeldiego3@users.noreply.github.com>
2025-12-10 02:56:11 -0300

    Estructura Base Laravel

---
+++ api_catalogo/resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ApiEV4</title>
+</head>
+<body>
+    <h1>API EV4</h1>
+
+    <style>
+        h1 {
+            color: #2c3e50;
+            font-family: "Times New Roman", serif;
+            text-align: center;
+            margin-top: 20%;
+            font-size: 200px;
+        }
+        background {
+            background-color: #ecf0f1;
+        }
+    </style>
+</body>
+</html>
